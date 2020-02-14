8da73c29912bff877664f5e4e5e2cf27bec38436
zypeh <zypeh.geek@gmail.com>
2020-02-14 16:14:48 +0800

    Renaming & test case added

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,8 @@ pub mod node;
 
 #[cfg(test)]
 mod tests {
-    use crate::node::*;
     use crate::node::Node::*;
-
+    use crate::node::*;
     #[test]
     fn atom_cmp() {
         assert_eq!(Atom("ratatouille"), Atom("ratatouille"));
@@ -12,58 +11,116 @@ mod tests {
     }
 
     #[test]
-    fn list_cmp() {
+    fn pair_cmp() {
         assert_eq!(
-            List(vec![Atom("ratatouille")]),
-            List(vec![Atom("ratatouille")])
+            PairNode(Pair {
+                0: Box::new(Atom("ratatouille")),
+                1: Box::new(Atom("ratatouille")),
+            }),
+            PairNode(Pair {
+                0: Box::new(Atom("ratatouille")),
+                1: Box::new(Atom("ratatouille")),
+            })
         );
 
         assert_ne!(
-            List(vec![Atom("ratatouille")]),
-            List(vec![Atom("baguette")])
+            PairNode(Pair {
+                0: Box::new(Atom("ratatouille")),
+                1: Box::new(Atom("ratatouille")),
+            }),
+            PairNode(Pair {
+                0: Box::new(Atom("ratatouille")),
+                1: Box::new(Atom("baguette")),
+            })
+        );
+
+        assert_ne!(
+            PairNode(Pair {
+                0: Box::new(Atom("ratatouille")),
+                1: Box::new(Atom("ratatouille")),
+            }),
+            PairNode(Pair {
+                0: Box::new(Atom("baguette")),
+                1: Box::new(Atom("ratatouille")),
+            })
+        );
+
+        assert_ne!(
+            PairNode(Pair {
+                0: Box::new(Atom("ratatouille")),
+                1: Box::new(Atom("baguette")),
+            }),
+            PairNode(Pair {
+                0: Box::new(Atom("baguette")),
+                1: Box::new(Atom("baguette")),
+            })
+        );
+
+        assert_ne!(
+            PairNode(Pair {
+                0: Box::new(Atom("baguette")),
+                1: Box::new(Atom("baguette")),
+            }),
+            PairNode(Pair {
+                0: Box::new(Atom("baguette")),
+                1: Box::new(Atom("ratatouille")),
+            })
         );
     }
 
     #[test]
-    fn list_cons() {
-        use crate::node::cons;
+    fn pair_cons() {
         assert_eq!(
-            cons(vec![Atom("ratatouille")], "baguette"),
-            List(vec![Atom("ratatouille"), Atom("baguette")])
+            Pair::cons(Atom("ratatouille"), Atom("baguette")),
+            Pair {
+                0: Box::new(Atom("ratatouille")),
+                1: Box::new(Atom("baguette")),
+            }
         )
     }
 
     #[test]
-    fn list_car() {
-        use crate::node::car;
+    fn pair_car() {
         assert_eq!(
-            car(vec![Atom("ratatouille"), Atom("baguette")]),
+            Pair::car(Pair {
+                0: Box::new(Atom("ratatouille")),
+                1: Box::new(Atom("baguette")),
+            }),
             Atom("ratatouille")
         );
     }
 
     #[test]
-    fn list_cdr() {
-        use crate::node::cdr;
+    fn pair_cdr() {
         assert_eq!(
-            cdr(vec![Atom("ratatouille"), Atom("baguette")]),
-            List(vec![Atom("baguette")])
+            Pair::cdr(Pair {
+                0: Box::new(Atom("ratatouille")),
+                1: Box::new(Atom("baguette")),
+            }),
+            Atom("baguette")
         );
 
         assert_eq!(
-            cdr(vec![
-                Atom("ratatouille"),
-                Atom("baguette"),
-                Atom("aubergine")
-            ]),
-            List(vec![Atom("baguette"), Atom("aubergine")])
+            Pair::cdr(Pair {
+                0: Box::new(Atom("ratatouille")),
+                1: Box::new(Node::PairNode(Pair {
+                    0: Box::new(Atom("baguette")),
+                    1: Box::new(Atom("aubergine")),
+                }))
+            }),
+            Node::PairNode(Pair {
+                0: Box::new(Atom("baguette")),
+                1: Box::new(Atom("aubergine")),
+            })
         )
     }
 
-    #[test]
-    fn type_of_term() {
-        assert_eq!(type_of(Atom("yo")), Type(0));
-        assert_eq!(type_of(List(vec![])), Type(0));
-        assert_eq!(type_of(Type(0)), Type(1))
-    }
+    // #[test]
+    // fn type_of_term() {
+    //     use crate::node::type_of;
+    //     use crate::node::Type;
+    //     assert_eq!(type_of(Atom("yo")), Type::Atom);
+    //     // assert_eq!(type_of(List(vec![])), Type::Pair());
+    //     assert_eq!(type_of(Type)), Type::Universe(1));
+    // }
 }
